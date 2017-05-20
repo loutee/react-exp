a818d70c61ce310d06f264cf825493bfe6556ac8
Louis Tran <loutran987@gmail.com>
2017-05-19 17:36:45 -0700

    Add React Router and Dashboard component

--- src/components/Login.js
+++
@@ -1,6 +0,0 @@
-export const Login = () => (
-  <div>
-		<h1>Sign In</h1>
-    <div className="g-signin2" data-onsuccess="onSignIn"></div>
-  </div>
-)
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import App from './components/App';
+import Dashboard from './components/Dashboard';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <div>
+      <Route path="/" component={App} />
+      <Route path="/dashboard" component={Dashboard} />
+    </div>
+  </Router>,
   document.getElementById('root')
 );
