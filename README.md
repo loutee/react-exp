52aa884587b457826b19adb49f9cb0f70e686d5e
Louis Tran <loutran987@gmail.com>
2017-05-26 23:01:02 -0700

    Clean TableA.js code

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Experimental React App
   Uses Firebase email and password login feature.
-  Produced from following code from https://github.com/tylermcginnis/react-router-firebase-auth
+  Login structure produced from following code from https://github.com/tylermcginnis/react-router-firebase-auth
 
 #### Note: Firebase API constants in /config should be provided by your own variables within a json file.
--- src/components/protected/TableA.js
+++ src/components/protected/TableA.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react'
 import { db } from '../../config/constants'
 
 export default class TableA extends Component {
+
   constructor(props) {
     super(props);
       this.state = { items: []};
   }
+
   componentWillMount() {
     let aitemsRef = db.ref('a-items')
     aitemsRef.on('child_added', snapshot => {
@@ -14,44 +16,44 @@ export default class TableA extends Component {
                     vitamins: snapshot.child('vitamins').val(),
                     recipes: snapshot.child('recipes').val()
                   };
-      //let aName = { id: snapshot.key, name: snapshot.child('name').val() };
-      this.setState({
-        items: [aItem].concat(this.state.items)
-        //names: [aName].concat(this.state.names)
-      });
+      this.setState({items: [aItem].concat(this.state.items)});
     })
   }
+
   render() {
     return (
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Vitamins</th>
-            <th>Recipes</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.items.map(
-              aItem =>
-              <tr>
-                <td key={aItem.id}>
-                  {aItem.id}
-                </td>
-                <td>
-                  {aItem.name}
-                </td>
-                <td>
-                  {aItem.vitamins}
-                </td>
-                <td>
-                  {aItem.recipes}
-                </td>
-              </tr>
-          )}
-        </tbody>
-      </table>
+      <div>
+        <button className="btn btn-default">Add Item</button>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Vitamins</th>
+              <th>Recipes</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.items.map(
+                aItem =>
+                <tr key={aItem.id}>
+                  <td>
+                    {aItem.id}
+                  </td>
+                  <td>
+                    {aItem.name}
+                  </td>
+                  <td>
+                    {aItem.vitamins}
+                  </td>
+                  <td>
+                    {aItem.recipes}
+                  </td>
+                </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
     )
   }
 }
